4e6b35d85ac94b2f495d07ee63fc5bfa61c652ad
Ackara <ackara.dev@outlook.com>
2016-01-06 23:34:38 -0400

    setup project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mockaroo.NET
+Mockaroo.NET is a portable class library that allows you to generate sample data based on your C# objects using the Mockaroo API.
---
+++ src/Tests/Tests.Mockaroo.NET/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Tests.Mockaroo.NET
+{
+    public struct Dev
+    {
+        public const string Ackara = "ackara.dev@outlook.com";
+    }
+
+    public struct Artifact
+    {
+    }
+}
