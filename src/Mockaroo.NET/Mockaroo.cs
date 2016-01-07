b02372d5a1400f00206a871bd3a536bcb9a66229
Ackara <ackara.dev@outlook.com>
2016-01-07 01:46:32 -0400

    checkpoint: create client classes.

---
+++ src/Mockaroo.NET/Mockaroo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Gigobyte.Mockaroo
+{
+    public static class Mockaroo
+    {
+        public static IEnumerable<T> FetchData<T>(string apiKey, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
