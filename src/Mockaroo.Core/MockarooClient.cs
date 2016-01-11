6bebfd930ee299b2c7a5fb8a6fe247fda0cb92fd
Ackara <ackara.dev@outlook.com>
2016-01-11 09:34:12 -0400

    rename parameters

--- src/Mockaroo.Core/MockarooClient.cs
+++ src/Mockaroo.Core/MockarooClient.cs
@@ -19,44 +19,51 @@ namespace Gigobyte.Mockaroo
             _serializer = serializer;
         }
 
-        public async Task<IEnumerable<T>> FetchDataAsync<T>(int rows)
+        public async Task<IEnumerable<T>> FetchDataAsync<T>(int records)
         {
-            return (await FetchDataAsync(typeof(T), new Schema(Schema.GetFields(typeof(T))), rows)).Cast<T>();
+            return (await FetchDataAsync(typeof(T), new Schema(Schema.GetFields(typeof(T))), records)).Cast<T>();
         }
 
-        public async Task<IEnumerable<object>> FetchDataAsync(Type returnType, int rows)
+        public async Task<IEnumerable<T>> FetchDataAsync<T>(Schema schema, int records)
         {
-            return await FetchDataAsync(returnType, new Schema(Schema.GetFields(returnType)), rows);
+            return (await FetchDataAsync(typeof(T), schema, records)).Cast<T>();
         }
 
-        public async Task<IEnumerable<object>> FetchDataAsync(Type returnType, Schema schema, int rows)
+        public async Task<IEnumerable<object>> FetchDataAsync(Type returnType, int records)
+        {
+            return await FetchDataAsync(returnType, new Schema(Schema.GetFields(returnType)), records);
+        }
+
+        public async Task<IEnumerable<object>> FetchDataAsync(Type returnType, Schema schema, int records)
         {
             var data = new LinkedList<object>();
 
             using (var client = new HttpClient())
             {
-                string url = string.Format(_urlFormat, _apiKey, rows, "json");
+                string url = string.Format(_urlFormat, _apiKey, records, "json");
                 string requestBody = schema.ToJson();
 
-                var response = await client.PostAsync(url, new StringContent(requestBody));
-                if (response.IsSuccessStatusCode)
+                using (var response = await client.PostAsync(url, new StringContent(requestBody)))
                 {
-                    string json = await response.Content.ReadAsStringAsync();
-                    foreach (JObject obj in JArray.Parse(json))
+                    if (response.IsSuccessStatusCode)
                     {
-                        data.AddLast(_serializer.Deserialize(obj, returnType));
+                        string json = await response.Content.ReadAsStringAsync();
+                        foreach (JObject obj in JArray.Parse(json))
+                        {
+                            data.AddLast(_serializer.Deserialize(obj, returnType));
+                        }
                     }
+                    else throw new System.Net.WebException($"[Status={response.StatusCode}]: {response.ReasonPhrase}");
                 }
-                else throw new System.Net.WebException($"[Status={response.StatusCode}]: {response.ReasonPhrase}");
             }
             return data;
         }
 
-        public async Task<System.IO.Stream> FetchDataAsync(Schema schema, int rows, ResponseFormat format = ResponseFormat.JSON)
+        public async Task<System.IO.Stream> FetchDataAsync(Schema schema, int records, ResponseFormat format = ResponseFormat.JSON)
         {
             using (var client = new HttpClient())
             {
-                string url = string.Format(_urlFormat, _apiKey, rows, format).ToLower();
+                string url = string.Format(_urlFormat, _apiKey, records, format).ToLower();
                 string requestBody = schema.ToJson();
 
                 var response = await client.PostAsync(url, new StringContent(requestBody));
