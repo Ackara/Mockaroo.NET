1b5047541dbf3e99ba7e118056b2b03184b5b44a
Ackara <ackara.dev@outlook.com>
2016-07-13 06:53:46 -0400

    Add limiter to JsonArrayField min and max properties

--- src/Mockaroo.Core/Fields/JSONArrayField.cs
+++ src/Mockaroo.Core/Fields/JSONArrayField.cs
@@ -2,13 +2,27 @@
 {
     public partial class JSONArrayField
     {
-        public int Min { get; set; } = 1;
+        public int Min
+        {
+            get { return _min; }
+            set { _min = value.Between(0, 100); }
+        }
 
-        public int Max { get; set; } = 5;
+        public int Max
+        {
+            get { return _max; }
+            set { _max = value.Between(0, 100); }
+        }
 
         public override string ToJson()
         {
             return $"{base.BaseJson()},\"minItems\":{Min},\"maxItems\":{Max}}}";
         }
+
+        #region Private Members
+
+        private int _min = 1, _max = 5;
+
+        #endregion Private Members
     }
 }
