f5eeea73594a04681f4b8ef7711e59ffa40a5f92
Ackara <ackara.dev@outlook.com>
2016-07-15 07:38:11 -0400

    Split ISchemaSerializer

---
+++ src/Mockaroo.Core/Serialization/ClrMockarooSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gigobyte.Mockaroo.Serialization
+{
+    public class ClrMockarooSerializer : IMockarooSerializer
+    {
+        public object[] ReadObject(Type type, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ReadObject<T>(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Tests/Tests.Mockaroo/UnitTest/ClrMockarooSerializerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Mockaroo.UnitTest
+{
+    [TestClass]
+    public class ClrMockarooSerializerTest
+    {
+        
+        public void MyTestMethod()
+        {
+            
+        }
+    }
+}
