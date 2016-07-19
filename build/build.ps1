46d24f1455aa81025866c6e5aa113ef5a2618b11
Ackara <ackara.dev@outlook.com>
2016-07-19 19:50:48 -0400

    Update build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -1,50 +1,60 @@
 Properties {
     # Credentials
-    $NuGetKey = $null;
+    $NuGetKey = "";
+
+    $Cloudinary_CloudName = "";
+    $Cloudinary_APIKey = "";
+    $Cloudinary_Secret = "";
     
-    # Paths
+    # Paths and URIs
     $ProjectDirectory = (Split-Path $PSScriptRoot -Parent);
-    $DeployDirectory = "$ProjectDirectory\build\bin\nuget";
-    $TempDirectory = "$ProjectDirectory\build\Temp";
+    $TempDirectory = "$ProjectDirectory\build\temp";
+    $BinDirectory = "$ProjectDirectory\build\bin";
+    
+    $NuGetSource = "https://www.nuget.org/api/v2/package";
     $NugetEXE = "$ProjectDirectory\tools\nuget.exe";
+    $NuGetPackageDirectory = "$BinDirectory\\nupkg";
+    $NuGetIcon = "";
     
     # Msbuild Args
     $BuildConfiguration = "Release";
     $BuildPlatform = "Any CPU";
 }
 
-Task default -depends Init, Create-NuGetPackages, Publish-NuGetPackages;
+Task default -depends Init, Compile, Push-NuGetIconToCDN, Publish-NuGetPackages;
 
 Task Init -description "Initialize the build n' deploy procedure." -action {
     Assert(Test-Path $ProjectDirectory -PathType Container) "`$ProjectionDirectory cannot be null or empty.";
+    Import-Module ((Get-ChildItem "$ProjectDirectory\src\packages\Gigobyte.DevOps.*\tools\Gigobyte.DevOps.dll").FullName | Sort-Object $_ | Select-Object -Last 1);
 
     # Cleanup directories.
-        foreach($dir in @($DeployDirectory))
+    foreach($dir in @($BinDirectory, $NuGetPackageDirectory, $TempDirectory))
+    {
+        if(Test-Path $dir -PathType Container)
         {
-            if(Test-Path $dir -PathType Container)
-            {
-               Remove-Item $dir -Force -Recurse;
-               New-Item $dir -ItemType Directory | Out-Null;
-            }
-            else { New-Item $dir -ItemType Directory | Out-Null; }
+            Remove-Item $dir -Force -Recurse;
+            New-Item $dir -ItemType Directory | Out-Null;
         }
+        else { New-Item $dir -ItemType Directory | Out-Null; }
+    }
 }
 
 Task Compile -description "Build the solution." -depends Init -action {
-    Assert("Debug", "Release" -contains $BuildConfiguration) "Value must be 'Debug' or 'Release'.";
-    Assert("x86", "x64", "Any CPU" -contains $BuildPlatform) "Value must be 'x86', 'x64' or 'Any CPU'.";
+    Assert("Debug", "Release" -contains $BuildConfiguration) "'BuildConfiguration' must be 'Debug' or 'Release'.";
+    Assert("x86", "x64", "Any CPU" -contains $BuildPlatform) "'BuildPlatform' must be 'x86', 'x64' or 'Any CPU'.";
 
     # Build Visual Studio solution.
-        $solution = (Get-ChildItem $ProjectDirectory -Recurse -Filter "*.sln" | Select-Object -ExpandProperty FullName -First 1);
-        Exec { msbuild $solution "/p:Configuration=$BuildConfiguration;Platform=$BuildPlatform" | Out-Null }
+    $solution = (Get-ChildItem $ProjectDirectory -Recurse -Filter "*.sln" | Select-Object -ExpandProperty FullName -First 1);
+    Exec { msbuild $solution "/p:Configuration=$BuildConfiguration;Platform=$BuildPlatform" | Out-Null }
 }
 
 Task Create-NuGetPackages -description "Create a nuget package for all non test projects." -depends Compile -action {
-    Push-Location $DeployDirectory;
+    Assert(Test-Path $NuGetPackageDirectory -PathType Container) "Could not find '$NuGetPackageDirectory'.";
+
+    Push-Location $NuGetPackageDirectory;
     try
     {
-        $projects = Get-ChildItem "$ProjectDirectory\src" -Recurse -Exclude @("Test*.csproj") -Filter "*.csproj" | Select-Object -ExpandProperty FullName;
-        foreach($project in $projects)
+        foreach($project in (Get-ChildItem "$ProjectDirectory\src" -Recurse -Filter "*.csproj" -Exclude @("*Test*") | Select-Object -ExpandProperty FullName))
         {
             $nuspec = [System.IO.Path]::ChangeExtension($project, ".nuspec");
             if(Test-Path $nuspec -PathType Leaf)
@@ -57,10 +67,26 @@ Task Create-NuGetPackages -description "Create a nuget package for all non test 
 }
 
 Task Publish-NuGetPackages -description "Publish nuget packages to nuget.org" -depends Create-NuGetPackages -action {
-    Assert(-not [System.String]::IsNullOrEmpty($NuGetKey)) "The 'NugetKey' cannot be null or empty.";
-
-    foreach($package in (Get-ChildItem $DeployDirectory | Select-Object -ExpandProperty FullName))
+    Assert(-not [System.String]::IsNullOrEmpty($NuGetSource)) "The 'NuGetSource' cannot be null nor empty.";
+    Assert(-not [System.String]::IsNullOrEmpty($NuGetKey)) "The 'NugetKey' cannot be null nor empty.";
+    
+    foreach($package in (Get-ChildItem $NuGetPackageDirectory | Select-Object -ExpandProperty FullName))
     {
-        Exec { & $NugetEXE push $($package) $($NuGetKey) -Source "https://www.nuget.org"; }
+        Exec { & $NugetEXE push $($package) $($NuGetKey) -Source $($NuGetSource); }
+    }
+}
+
+Task Push-NuGetIconToCDN -description "Upload an image to https://cloudinary.com CDN." -action {
+    Assert(-not [String]::IsNullOrEmpty($Cloudinary_APIKey)) "'Cloudinary_APIKey' cannot be null nor empty.";
+    Assert(-not [String]::IsNullOrEmpty($Cloudinary_Secret)) "'Cloudinary_Secret' cannot be null nor empty.";
+    Assert(-not [String]::IsNullOrEmpty($Cloudinary_CloudName)) "'Cloudinary_CloundName' cannot be null nor empty.";
+    
+    Write-Host "`t* Uploading 'mockaroo-net.png' to 'https://api.cloudinary.com'...";
+    $imageUrl = Push-ImageToCloudinary "$ProjectDirectory\mockaroo-net.png" -CloudName $Cloudinary_CloudName -APIKey $Cloudinary_APIKey -Secret $Cloudinary_Secret;
+
+    Write-Host "`t* Replacing all .nuspec files <iconUrl> element with image url...";
+    foreach($nuspec in (Get-ChildItem "$ProjectDirectory\src" -Recurse -Filter "*.nuspec" | Select-Object -ExpandProperty FullName))
+    {
+        Edit-Xml $nuspec -XPath "package/metadata/iconUrl" -Value $imageUrl;
     }
 }
