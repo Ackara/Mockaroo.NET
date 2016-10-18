c42572419855bcdce45d8bea99664c0f63089aae
Ackara <ackara.dev@outlook.com>
2016-10-18 07:37:45 -0400

    Add commands to task runner

--- tools/Update-VersionNumber.ps1
+++ tools/Update-VersionNumber.ps1
@@ -1,2 +1,25 @@
 <#
+
+.SYSNOPSIS
+This 
+
+
 #>
+
+[CmdletBinding()]
+Param(
+
+)
+
+function GetRevisionNumber()
+{
+	
+	return 1;
+}
+
+$major = 1;
+$minor = 1;
+$build = [Convert]::ToInt32([DateTime]::UtcNow.ToString("yyMMdd"));
+$revision = GetRevisionNumber;
+$version = "$major.$minor.$build.$revision";
+echo $version;
